63bd52cfaf3b3a946530c65fac0f4afcf37e1c2c
mathewcandido <matheuscandidoo70@gmail.com>
2023-12-12 00:38:16 -0300

    added cors

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const { findAll, create, deleteitem, update, findById } = require('./controllers/RoutersController')
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 const gate = 8000;
+
+
+
+const allowedOrigins = ['http://localhost:3000'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
 
 // Rota para obter todas as postagens
 app.get("/api/posts", findAll);
